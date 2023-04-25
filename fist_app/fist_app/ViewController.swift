ccc8de9ae0e596db0fc68a182c0d1e9d28159a26
truong.thien <95615938+nguyenhuutu270402@users.noreply.github.com>
2023-04-25 16:51:28 +0700

    update 2

--- fist_app/fist_app/ViewController.swift
+++ fist_app/fist_app/ViewController.swift
@@ -32,12 +32,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        tao text label
-        let myLabel = UILabel(frame: CGRect(x: 150, y: 100, width: 100, height: 30))
-        myLabel.text="My label"
+        let myLabel = UILabel()
+        myLabel.text = "My label Ví dụ, nếu bạn muốn đặt chiều dài của một view bằng với chiều rộng của một view khác, bạn "
         myLabel.textColor = UIColor.blue
         myLabel.textAlignment = .center
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        myLabel.numberOfLines = 0
+
+        // Add label to view
         self.view.addSubview(myLabel)
-        
+
+        // Add constraints to label
+        NSLayoutConstraint.activate([
+            myLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
+            myLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 100),
+            myLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -70),
+           
+        ])
+
+
+
 //        tao button
         let okButton = UIButton(frame: CGRect(x: 150, y: 300, width: 100, height: 50))
         okButton.setTitle("OK", for: .normal)
@@ -47,6 +61,22 @@ class ViewController: UIViewController {
         okButton.layer.cornerRadius = 10
         okButton.addTarget(self, action: #selector(okButton_clicked), for: .touchUpInside)
         self.view.addSubview(okButton)
+        
+        myImage.layer.cornerRadius = 10
+//        myImage.image = UIImage(data: try! Data(contentsOf: URL(string: "https://bizweb.dktcdn.net/100/303/962/files/87126502-2509242206005371-2073523065622364160-n-f697e400-e8b2-4bb1-9698-d00b50b2d9c3.jpg?v=1627804121650")!))
+        if let url = URL(string: "https://bizweb.dktcdn.net/100/303/962/files/87126502-2509242206005371-2073523065622364160-n-f697e400-e8b2-4bb1-9698-d00b50b2d9c3.jpg?v=1627804121650") {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.myImage.image = UIImage(data: data!)
+                }
+            }
+            task.resume()
+        }
+
     }
     
     
